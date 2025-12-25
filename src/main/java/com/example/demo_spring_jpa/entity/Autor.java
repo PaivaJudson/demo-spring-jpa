cefb5573996ad6b5d5216c5d48ccbf16429a552c
Judson Paiva <judson.paiva@hotmail.com>
2025-12-25 21:03:21 +0100

    first commit - anotacoes JPA

---
+++ src/main/java/com/example/demo_spring_jpa/entity/Autor.java
@@ -0,0 +1,15 @@
+package com.example.demo_spring_jpa.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "autores")
+public class Autor implements Serializable {
+
+
+
+
+
+}
